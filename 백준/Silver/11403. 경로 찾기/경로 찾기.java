0bc99382a0c9471005c576a50194d42178721634
hwi-lee <riteru66@naver.com>
2025-01-31 13:57:35 +0900

    [Silver I] Title: 경로 찾기, Time: 288 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int[][] graph;
+
+	public static void floyd_warshall() {
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (graph[i][k] == 1 && graph[k][j] == 1)
+						graph[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		graph = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+
+			}
+		}
+		floyd_warshall();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(graph[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
