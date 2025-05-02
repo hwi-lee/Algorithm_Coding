22c583a99a0de697ab5b584b1d7828f06a7c197b
hwi-lee <riteru66@naver.com>
2025-05-02 17:10:52 +0900

    [Gold V] Title: Z, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	static int N;
+	static int r, c, cnt;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		r = sc.nextInt();
+		c = sc.nextInt();
+		cnt = 0;
+		z(0, 0, 1 << N);// 1<<N == 2^N
+
+	}
+
+	public static void z(int rr, int cc, int width) {
+
+		if (rr == r && cc == c) {
+			System.out.println(cnt);
+
+			return;
+		}
+		if (r >= rr && r < rr + width && c >= cc && c < cc + width) {
+			int w = width / 2;
+			z(rr, cc, w);
+			z(rr, cc + w, w);
+			z(rr + w, cc, w);
+			z(rr + w, cc + w, w);
+
+		} else
+			cnt += width * width;
+
+	}
+
+}
