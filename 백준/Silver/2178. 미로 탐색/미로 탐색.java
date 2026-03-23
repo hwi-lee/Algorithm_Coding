6a5c28eab5d09b3909543191e081d8960fd5aa3a
hwi-lee <riteru66@naver.com>
2026-03-23 17:13:28 +0900

    [Silver I] Title: 미로 탐색, Time: 160 ms, Memory: 16740 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,15 +1,21 @@
-import java.awt.*;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N, M;
+	static int[][] graph;
+	static int[] dr = { -1, 0, 1, 0 }, dc = { 0, 1, 0, -1 };
 
-	static void bfs(int[][] a, int i, int j) {
-		Queue q = new LinkedList();
-		q.offer(new int[] { i, j });
-
+	static void bfs(int r, int c) {
+		Queue<int[]> q = new LinkedList<int[]>();
+		q.offer(new int[] { r, c });
 		while (!q.isEmpty()) {
 			int[] p = (int[]) q.poll();
 			int n = p[0];
@@ -17,26 +23,21 @@ public class Main {
 			if (n + 1 == N && m + 1 == M) {
 				break;
 			}
-			if (n - 1 >= 0 && a[n - 1][m] == 1) {
-				a[n - 1][m] = a[n][m] + 1;
-				q.offer(new int[] { n - 1, m });
-			}
-			if (m - 1 >= 0 && a[n][m - 1] == 1) {
-				a[n][m - 1] = a[n][m] + 1;
-				q.offer(new int[] { n, m - 1 });
-			}
-			if (n + 1 < N && a[n + 1][m] == 1) {
-				a[n + 1][m] = a[n][m] + 1;
-				q.offer(new int[] { n + 1, m });
-			}
-			if (m + 1 < M && a[n][m + 1] == 1) {
-				a[n][m + 1] = a[n][m] + 1;
-				q.offer(new int[] { n, m + 1 });
+			for (int i = 0; i < 4; i++) {
+				if (n + dr[i] < 0 || n + dr[i] >= N || m + dc[i] < 0 || m + dc[i] >= M)
+					continue;
+				if (graph[n + dr[i]][m + dc[i]] == 1) {
+					q.offer(new int[] { n + dr[i], m + dc[i] });
+					graph[n + dr[i]][m + dc[i]] = graph[n][m] + 1;
+				}
+
 			}
 		}
+
 	}
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -44,16 +45,16 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		int a[][] = new int[N][M];
+		graph = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			String s[] = br.readLine().split("");
 			for (int j = 0; j < M; j++) {
-				a[i][j] = Integer.parseInt(s[j]);
+				graph[i][j] = Integer.parseInt(s[j]);
 			}
 		}
 
-		bfs(a, 0, 0);
-		System.out.println(a[N - 1][M - 1]);
+		bfs(0, 0);
+		System.out.println(graph[N - 1][M - 1]);
 
 	}
 
